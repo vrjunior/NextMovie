fd65fd62cf38c1edc7abfe0bec9f97675b470573
Valmir Junior <valmirmassoni@gmail.com>
2018-11-10 09:21:16 -0200

    White status bar

--- NextMovie/NextMovie/AppDelegate.swift
+++ NextMovie/NextMovie/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //change the status bar to light style
+        UINavigationBar.appearance().barStyle = UIBarStyle.blackOpaque
+        
         return true
     }
 
